26a89e8195f2cffe6299a222ea6f15f449f301bd
Melinda Winter <melindawinter42@gmail.com>
2020-08-03 11:43:27 -0700

    create switch statements

--- index.js
+++ index.js
@@ -23,24 +23,78 @@ connection.connect(function (err) {
 
 // This first function sets up the task the user wants to perform
 function initialQuestion() {
-  inquirer.prompt({
-    type: "list",
-    name: "manage",
-    message: "Hello. Which management task would you like to perform?",
-    choices: [
-      "View all employees",
-      "View all employees by department",
-      "View all employees by manager",
-      "Add an employee",
-      "Remove an employee",
-      "Update an employee's role",
-      "Update an employee's manager",
-    ],
-  }).then(answers => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "manage",
+      message: "Hello. Which management task would you like to perform?",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add an employee",
+        "Remove an employee",
+        "Update an employee's role",
+        "Update an employee's manager",
+        "Quit",
+      ],
+    })
+    .then(answers => {
       console.log(answers.manage);
-    // Switch statement to redirect to next portion of the questionnaire depending on the answer
-    switch(answers.manage) {
-        case
-    }
-  })
+      // Switch statement to redirect to next portion of the questionnaire depending on the answer
+      switch (answers.manage) {
+        case "View all employees":
+          viewAll();
+          break;
+
+        case "View all employees by department":
+          viewDepartment();
+          break;
+
+        case "View all employees by manager":
+          viewManager();
+          break;
+
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Remove an employee":
+          removeEmployee();
+          break;
+
+        case "Update an employee's role":
+          updateRole();
+          break;
+
+        case "Update an employee's manager":
+          updateManager();
+          break;
+
+        default:
+          connection.end();
+          break;
+      }
+    });
 }
+
+// Function to view all employees
+function viewAll() {
+  connection.query("SELECT * FROM employee", function (err, data) {
+    console.table(data);
+    // Call function to go back to the questionnaire
+    initialQuestion();
+  });
+}
+
+// function viewDepartment
+
+// function viewManager
+
+// function addEmployee
+
+// function removeEmployee
+
+// function updateRole
+
+// function updateManager
